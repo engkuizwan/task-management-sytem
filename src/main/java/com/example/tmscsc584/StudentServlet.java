1e0d2d33ea0dae9edd854dbb7005cb4a3b927999
ku <izwan2010@gmail.com>
2022-02-05 17:35:02 +0800

    student - join class

--- src/main/java/com/example/tmscsc584/StudentServlet.java
+++ src/main/java/com/example/tmscsc584/StudentServlet.java
@@ -191,9 +191,9 @@ public class StudentServlet extends HttpServlet {
 
     }
 
-/*    *//*######################################################( QUIT CLASS )#############################################################*//*
+    /*######################################################( QUIT CLASS )#############################################################*/
 
-    private void quitclass(HttpServletRequest request, HttpServletResponse response)
+    /*private void quitclass(HttpServletRequest request, HttpServletResponse response)
             throws SQLException, IOException {
         int id = Integer.parseInt(request.getParameter("classid"));
         Classs classs = new Classs();
@@ -205,12 +205,12 @@ public class StudentServlet extends HttpServlet {
         response.sendRedirect("Student-viewclass.jsp");
         request.getRequestDispatcher("Student-viewclass.jsp").forward(request,response);}catch (Exception e){e.printStackTrace();}
 
-    }
-
-
-
-
     }*/
+
+
+
+
+    }
 
 
 
